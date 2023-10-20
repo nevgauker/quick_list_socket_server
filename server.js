3229faece15ad26b95b752a7ee617026f846fc91
rotem <nevgauker@gmail.com>
2023-10-20 17:04:21 +0300

    all filess

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.use(express.static(__dirname + '/public'));
+
+io.on('connection', (socket) => {
+  console.log('A user connected.');
+
+  socket.on('newObject', (data) => {
+    // Handle new JSON object here
+    console.log('Received new object:', data);
+    // Broadcast the new object to all connected clients
+    io.emit('newObject', data);
+  });
+
+  socket.on('updateObject', (data) => {
+    // Handle updated JSON object here
+    console.log('Received updated object:', data);
+    // Broadcast the updated object to all connected clients
+    io.emit('updateObject', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected.');
+  });
+});
+
+const port = process.env.PORT || 3333;
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
